830cabe5878b681d382880e5102b2372bdbef19e
Rafael Delboni <rafadelboni@gmail.com>
2023-12-08 17:20:07 -0300

    feat: adds day 3 helper functions

--- src/challenges/day-03.lisp
+++ src/challenges/day-03.lisp
@@ -1,7 +1,67 @@
 (defpackage :adventofcode-2023.challenges.day-03
-  (:use :cl :arrow-macros)
-  (:export :main))
+  (:use :cl :uiop :arrow-macros :cl-ppcre)
+  (:export :main
+           :partitions
+           :idx-in-range
+           :idx-in-range-remove
+           :lines->range-number-alist
+           :map-index
+           :get-search-range))
 (in-package :adventofcode-2023.challenges.day-03)
+
+(defun partitions (input-list size &optional out)
+  (if (> (length input-list) 0)
+    (partitions (nthcdr size input-list)
+                size
+                (if (> (length input-list) size)
+                  (push (subseq input-list 0 size) out)
+                  (push (subseq input-list 0 (length input-list)) out)))
+    out))
+
+(defun lines->range-number-alist (lines)
+  (->> lines
+       (mapcar #'(lambda (line)
+                   (let ((matches (cl-ppcre:all-matches "\\d+" line)))
+                     (when matches
+                       (pairlis
+                         (reverse (partitions matches 2))
+                         (cl-ppcre:all-matches-as-strings "\\d+" line))))))))
+
+;; (setq sample
+;;       (list "467..114.."
+;;             "...*......"
+;;             "..35..633."
+;;             "......#..."
+;;             "617*......"
+;;             ".....+.58."
+;;             "..592....."
+;;             "......755."
+;;             "...$.*...."
+;;             ".664.598.."))
+;; (lines->range-number-alist sample)
+
+(defun idx-in-range (index range)
+  (and (>= index (first range))
+       (<= index (first (cdr range)))))
+
+(defun idx-in-range-remove (index range)
+  (and (>= index (first (first range)))
+       (<= index (first (last (first range))))))
+
+(defun map-index (type fn &rest seqs)
+  (let ((index -1))
+    (apply #'map
+           type
+           (lambda (&rest args)
+             (incf index)
+             (apply fn index args))
+           seqs)))
+
+(defun get-search-range (index max-lenght)
+  (remove-if-not #'identity
+             (list (when (>= (1- index) 0) (1- index))
+                   index
+                   (when (<= (1+ index) max-lenght) (1+ index)))))
 
 (defun main (part)
   (let ((value part))
--- tests/challenges/test-day-03.lisp
+++ tests/challenges/test-day-03.lisp
@@ -9,8 +9,40 @@
 
 (in-suite test-day-03)
 
-(test test-dummy
-      (is (= 48
-             48)))
+(test test-partitions
+      (is (equal (partitions '(1 2 3 4 5 6 7 8 9) 2)
+                 '((9) (7 8) (5 6) (3 4) (1 2))))
+      (is (equal (partitions '(1 2 3 4 5 6 7 8 9) 3)
+                 '((7 8 9) (4 5 6) (1 2 3)))))
+
+(test test-idx-in-range
+      (is (equal (assoc 6 '(((5 8) . "114") ((0 3) . "467"))
+                        :test #'idx-in-range)
+                 '((5 8) . "114"))))
+
+(test test-idx-in-range-remove
+      (is (equal (remove 6 '(((5 8) . "114") ((0 3) . "467") ((9 12) . "123"))
+                         :test #'idx-in-range-remove)
+                 '(((0 3) . "467") ((9 12) . "123")))))
+
+(test test-lines->range-number-alist
+      (is (equal (lines->range-number-alist (list "467..114.."
+                                                  "...*......" "..35..633."))
+                 '((((5 8) . "114") ((0 3) . "467"))
+                   NIL
+                   (((6 9) . "633") ((2 4) . "35"))))))
+
+(test test-map-index
+      (is (equal (map-index 'list (lambda (x a) (cons x a))
+                            (list "467..114.." "...*......"))
+                 '((0 . "467..114..") (1 . "...*......")))))
+
+(test test-get-search-range
+      (is (equal (get-search-range 1 5)
+                 '(0 1 2)))
+      (is (equal (get-search-range 8 8)
+                 '(7 8)))
+      (is (equal (get-search-range 0 8)
+                 '(0 1))))
 
  ;; (run! 'test-day-03)
