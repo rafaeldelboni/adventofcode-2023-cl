5f1cc948ca3c43c439f12f9c2c88a3577763798f
Rafael Delboni <rafadelboni@gmail.com>
2023-12-08 08:37:45 -0300

    feat: adds day 2 part 2

--- src/challenges/day-02.lisp
+++ src/challenges/day-02.lisp
@@ -3,7 +3,9 @@
   (:export :main
            :game->colors
            :is-good-game
-           :sum-good-games))
+           :sum-good-games
+           :get-minimal-game
+           :sum-minimal-games))
 (in-package :adventofcode-2023.challenges.day-02)
 
 (defun flatten (l)
@@ -27,14 +29,15 @@
        (map 'list #'(lambda (cur)
                       (let ((color (first (last cur)))
                             (value (parse-integer (first cur))))
-                        (list color value))))))
+                        (cons color value))))))
 
+;; Part 1
 (defun is-good-game (game)
   (-<>> game
         #'game->colors
         (reduce #'(lambda (acc cur)
                     (let ((color (first cur))
-                          (value (first (last cur))))
+                          (value (cdr cur)))
                       (or acc 
                           (and (string= color "red")
                                (> value 12))
@@ -57,9 +60,33 @@
                             #'parse-integer)))
        (reduce #'+)))
 
+;; Part 2
+(defun get-minimal-game (game)
+  (-<>> game
+        #'game->colors
+        (reduce #'(lambda (acc cur)
+                    (let* ((color (first cur))
+                           (cur-value (cdr cur))
+                           (acc-value (cdr (assoc color acc :test #'string=))))
+                      (when (> cur-value (or acc-value 0))
+                        (rplacd (assoc color acc :test #'string=) cur-value))
+                      acc))
+                <> 
+                :initial-value (list (cons "green" NIL)
+                                     (cons "blue" NIL)
+                                     (cons "red" NIL)))
+        (reduce #'(lambda (acc cur) (* (cdr cur) acc)) <> :initial-value 1)))
+
+(defun sum-minimal-games (games)
+  (->> games
+       (map 'list #'get-minimal-game)
+       (reduce #'+)))
+
 (defun main (part)
   (let ((value part))
     (cond ((string= value '"part-1") (->> "resources/day-02/input.txt"
                                           #'uiop:read-file-lines
                                           #'sum-good-games))
-          ((string= value '"part-2") "not implemented"))))
+          ((string= value '"part-2") (->> "resources/day-02/input.txt"
+                                          #'uiop:read-file-lines
+                                          #'sum-minimal-games)))))
--- tests/challenges/test-day-02.lisp
+++ tests/challenges/test-day-02.lisp
@@ -15,7 +15,7 @@
 
 (test test-game->colors
       (is (equal (game->colors "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
-                 '(("blue" 3) ("red" 4) ("red" 1) ("green" 2) ("blue" 6) ("green" 2)))))
+                 '(("blue" . 3) ("red" . 4) ("red" . 1) ("green" . 2) ("blue" . 6) ("green" . 2)))))
 
 (test test-is-good-game
       (is-true (is-good-game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"))
@@ -26,5 +26,15 @@
                                    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
                                    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"))
              3)))
+
+(test test-get-minimal-game
+      (is (= (get-minimal-game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+             48)))
+
+(test test-sum-good-games
+      (is (= (sum-minimal-games (list "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+                                      "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+                                      "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"))
+             1620)))
 
 ;; (run! 'test-day-02)
